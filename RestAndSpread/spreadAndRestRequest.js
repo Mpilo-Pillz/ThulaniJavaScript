8701a0e0a4934aa178634312b6d1e274c1312a65
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-05-14 22:35:15 +0200

    combining 2 objects with the spread operator

--- RestAndSpread/spreadAndRestRequest.js
+++ RestAndSpread/spreadAndRestRequest.js
@@ -1,4 +1,5 @@
 const adminUser = {
+  id: 18,
   username: "admin",
   password: "admin",
   name: "Admin",
@@ -7,10 +8,19 @@ const adminUser = {
   lastName: "Admin",
 };
 
+const adminSkillz = {
+  adminId: 18,
+  skillz: ["JavaScript", "React", "Node"],
+  yearsOfExperience: 10,
+};
+
 const requestBody = {
   ...adminUser,
 };
 
+const adminCompleteProfile = { ...adminSkillz, ...adminUser };
+
 const { password, ...withoutPassword } = adminUser;
 
 console.log(withoutPassword);
+console.log("AdminComplete", adminCompleteProfile);
