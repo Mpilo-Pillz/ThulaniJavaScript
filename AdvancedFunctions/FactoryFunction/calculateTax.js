d566aaadf7263bf8c3d7ebc130f8ef51e844bc93
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-06-27 21:05:25 +0200

    revising Factory function

---
+++ AdvancedFunctions/FactoryFunction/calculateTax.js
@@ -0,0 +1,11 @@
+function calculateTax(tax) {
+  return function (amount) {
+    return amount * tax;
+  };
+}
+
+let calculateVAT = calculateTax(0.15);
+let calculateIncomeTax = calculateTax(0.2);
+
+console.log(calculateVAT(100000));
+console.log(calculateIncomeTax(10000));
