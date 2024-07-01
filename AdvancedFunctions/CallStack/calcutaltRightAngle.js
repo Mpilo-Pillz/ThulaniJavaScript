49e304eb202ad6e052659d2eb6b728d22de9465f
Fikasentani <mpilo.dlamini@yahoo.com>
2024-07-01 21:16:03 +0200

    using functional programming to check if it is a right angle triangle

--- AdvancedFunctions/CallStack/calcutaltRightAngle.js
+++ AdvancedFunctions/CallStack/calcutaltRightAngle.js
@@ -3,3 +3,14 @@ function calculateRightAngle(a, b, c) {
 }
 
 console.log(calculateRightAngle(3, 4, 5))
+
+
+const multiply = (b, c) => b * c;
+
+// const square = (a) => a * a;
+const square = (a) => multiply(a, a);
+
+const isRightAngle = (a, b, c) => square(a) + square(b) === square(c);
+console.log(isRightAngle(3, 4, 5))
+console.log(isRightAngle(6, 8, 10))
+console.log(isRightAngle(1, 2, 3))
