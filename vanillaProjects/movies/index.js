5e35c0edfad7b61e405ab0c47bc7dda9b18a5931
Fikasentani <mpilo.dlamini@yahoo.com>
2026-07-08 17:30:34 +0200

    creating a debounce

--- vanillaProjects/movies/index.js
+++ vanillaProjects/movies/index.js
@@ -2,15 +2,24 @@ const apiKey = ''
 const imdbID = "tt0848228"
 const fetchData = async (searchTerm) => {
     const movies = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`)
-    // console.log("Movies", await movies.json())
 }
 
 fecthMovieDetails = async () => {
     const movies = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`)
     console.log("Movies data", await movies.json())
 }
+let timeOutID;
+const onInput = (event) => {
+    if (timeOutID) {
+        clearInterval(timeOutID)
+        console.log("we have cancelled the pending code on the event loop")
+    }
+    console.log("===this will always run to create a new timeout")
+    timeOutID = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1000)
+}
 
-const input = document.querySelector('input', (event) => {
-    fetchData(event.target.value);
-})
+const input = document.querySelector('input')
+input.addEventListener('input', onInput)
 
