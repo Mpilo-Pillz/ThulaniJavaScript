def45f259a9cd70644e6a8657df6b29d63feb125
Fikasentani <mpilo.dlamini@yahoo.com>
2026-07-08 17:02:06 +0200

    adding input

--- vanillaProjects/movies/index.js
+++ vanillaProjects/movies/index.js
@@ -1,7 +1,16 @@
-
-const fetchData = async () => {
-    const movies = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=avengers`)
-    console.log("Movies", await movies.json())
+const apiKey = ''
+const imdbID = "tt0848228"
+const fetchData = async (searchTerm) => {
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`)
+    // console.log("Movies", await movies.json())
 }
 
-fetchData()
+fecthMovieDetails = async () => {
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`)
+    console.log("Movies data", await movies.json())
+}
+
+const input = document.querySelector('input', (event) => {
+    fetchData(event.target.value);
+})
+
