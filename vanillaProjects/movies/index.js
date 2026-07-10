9d26dfbcafbdcca36e810385c57cad533327f29b
Fikasentani <mpilo.dlamini@yahoo.com>
2026-07-10 09:37:08 +0200

    rendering the mvoeis

--- vanillaProjects/movies/index.js
+++ vanillaProjects/movies/index.js
@@ -24,6 +24,14 @@ fecthMovieDetails = async () => {
 const onInput = async (event) => {
     const movies = await fetchData(event.target.value);
     console.log("Movies==>", movies)
+    for (let movie of movies) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+        `
+        document.querySelector("#target").appendChild(div);
+    }
 };
 
 const input = document.querySelector("input", 500);
