e687aa1b4327fa05d60ee5a2418eea7b28a7530c
Fikasentani <mpilo.dlamini@yahoo.com>
2026-07-14 08:55:41 +0200

    correctly displating the results with styles

--- vanillaProjects/movies/index.js
+++ vanillaProjects/movies/index.js
@@ -1,43 +1,6 @@
 const apiKey = secret;
 const imdbID = "tt0848228";
 
-const fetchData = async (searchTerm) => {
-    const response = await fetch(
-        `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`
-    );
-    const data = await response.json()
-    if (data.Error) {
-        console.log("No movies found")
-        return []
-    }
-    return data.Search
-};
-
-fecthMovieDetails = async () => {
-    const response = await fetch(
-        `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`
-    );
-    console.log("Movies data", await response.json());
-
-    const data = await response.json()
-    return data.Search
-};
-
-
-
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
-    console.log("Movies==>", movies)
-    for (let movie of movies) {
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
-        `
-        document.querySelector("#target").appendChild(div);
-    }
-};
-
 const root = document.querySelector(".autocomplete")
 root.innerHTML = `
 <label><b>Search for a Movie</b></label>
@@ -52,21 +15,34 @@ const input = document.querySelector("input");
 const dropdown = document.querySelector(".dropdown")
 const resultsWrapper = document.querySelector(".results")
 
+const fetchData = async (searchTerm) => {
+    const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`
+    );
+    const data = await response.json()
+    if (data.Error) {
+        console.log("No movies found")
+        return []
+    }
+    return data.Search
+};
+
+
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+    dropdown.classList.add('is-active')
+    console.log("Movies==>", movies)
+    for (let movie of movies) {
+        const option = document.createElement('a')
+        option.innerHTML = `
+        <img src="${movie.Poster}" />
+        <p>${movie.Title}</p>
+        `
+        option.classList.add("dropdown-item")
+        resultsWrapper.appendChild(option);
+    }
+};
+
 input.addEventListener("input", debounce(onInput, 500));
 
-// REference:
-// let timeOutID;
-// const onInput = (event) => {
-//     if (timeOutID) {
-//         clearInterval(timeOutID) should me clearTimeout
-//         console.log("we have cancelled the pending code on the event loop")
-//     }
-//     console.log("===this will always run to create a new timeout")
-//     timeOutID = setTimeout(() => {
-//         fetchData(event.target.value);
-//     }, 1000)
-// }
 
-// const onInput = debounce((event) => {
-//   fetchData(event.target.value);
-// });
