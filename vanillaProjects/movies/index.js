d6dbcea7e7251c59c7995c15fe97c4646908fb93
Fikasentani <mpilo.dlamini@yahoo.com>
2026-07-19 19:53:12 +0200

    handling empty state

--- vanillaProjects/movies/index.js
+++ vanillaProjects/movies/index.js
@@ -32,6 +32,11 @@ const onInput = async (event) => {
     const movies = await fetchData(event.target.value);
     const placeholder = "https://media.istockphoto.com/id/1980276924/vector/no-photo-thumbnail-graphic-element-no-found-or-available-image-in-the-gallery-or-album-flat.jpg?s=612x612&w=0&k=20&c=ZBE3NqfzIeHGDPkyvulUw14SaWfDj2rZtyiKv3toItk="
 
+
+    if (!movies.length) {
+        dropdown.classList.remove('is-active')
+        return
+    }
     resultsWrapper.innerHTML = ''
     dropdown.classList.add('is-active')
 
